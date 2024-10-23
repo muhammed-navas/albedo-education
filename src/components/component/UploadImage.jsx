273ffa90e8c7c64ee6ec5489e2c66d5d6748040e
muhammed navas <muhammednavas6287@gmail.com>
2024-10-24 04:07:36 +0530

    add upload image file box in student add box

---
+++ src/components/component/UploadImage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { IoCameraOutline } from "react-icons/io5";
+
+export const UploadedImage = () => {
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-12 h-12 cursor-pointer rounded-full border-2 border-dashed border-gray-300 flex items-center justify-center">
+        {/* Make the entire div clickable */}
+        <label className="w-full h-full cursor-pointer flex items-center justify-center">
+          {image ? (
+            <img
+              src={URL.createObjectURL(image)}
+              alt="Uploaded"
+              className="w-full h-full rounded-full object-cover"
+            />
+          ) : (<>
+            {/* // Use label to trigger file input */}
+            <IoCameraOutline />
+            <input type="file" accept="image/*" onChange={handleImageUpload} hidden />
+            </>
+          )}
+        </label>
+      </div>
+      <div className="text-gray-500 pb-4">
+        <p className="text-xs text-center mt-2">Add your passport size photo</p>
+        <p className="text-xs text-center text-gray-400">JPG, JPEG, PNG, up to 5MB (1080x1080 px)</p>
+      </div>
+    </div>
+  );
+};
--- src/popup/StudentAddPopup.jsx
+++ src/popup/StudentAddPopup.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { PopupBackground } from '../children/popup/PopupBackground'
 import { ResponsiveGrid } from '../children/styleReuse/Style'
 import { Input } from '../components/component/Input'
+import { UploadedImage } from '../components/component/UploadImage'
 
 export const StudentAddPopup = () => {
   return (
     <PopupBackground >
         {/* <ResponsiveGrid lg="grid-cols-2" normal="grid-cols-1" > */}
+        <UploadedImage />
             <div className='grid grid-cols-2 gap-6'>
 
         <Input type='text' placeholder='first name' />
