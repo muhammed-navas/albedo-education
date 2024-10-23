a41016b79db7c31c354029ea4feda6ffc99814df
muhammed navas <muhammednavas6287@gmail.com>
2024-10-24 04:52:55 +0530

    new student add input setup not completed

--- src/children/popup/PopupBackground.jsx
+++ src/children/popup/PopupBackground.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const PopupBackground = ({ children }) => {
+export const PopupBackground = ({ children ,maxH }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-white rounded-2xl p-6 w-full max-w-md">
+    <div className={` fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50`}>
+      <div className={`${maxH} w-fit  bg-gray-200 overflow-y-scroll  rounded-2xl py-6 px-12 `}>
         {children}
       </div>
     </div>
